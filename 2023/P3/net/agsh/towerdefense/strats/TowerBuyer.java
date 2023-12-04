27e1c8bf68b8ba6bdb8f19f952417b1c6af8151a
Javiki <javipatinador@gmail.com>
2023-12-04 12:11:05 +0100

    Práctica 3 al menos funciona

--- 2023/P3/net/agsh/towerdefense/strats/TowerBuyer.java
+++ 2023/P3/net/agsh/towerdefense/strats/TowerBuyer.java
@@ -19,68 +19,47 @@ public class TowerBuyer {
         // The selected towers must be affordable, and the total cost must be less than or equal to money.
         // The indices should be given in the order that the towers are given in the original ArrayList<Tower> towers.
 
-        // Create an ArrayList<Integer> to store the indices of the towers you want to buy.
-
-
-//
-//        // Loop through the towers.
-//        for (int i = 0; i < towers.size(); i++) {
-//            // If the tower is affordable, buy it.
-//            if(money >= towers.get(i).getCost()) {
-//                // Subtract the cost of the tower from money.
-//                money -= towers.get(i).getCost();
-//                // Add the index of the tower to selected.
-//                selected.add(i);
-//            }
-//        }
-
-        ArrayList<Integer> selected = new ArrayList<>();
-        float [][] T = new float[towers.size()+1][(int) money + 1];
         int n = towers.size();
+        float[][] T = new float[n + 1][(int) (money + 1)];
 
-        for(int i=0; i<=n; i++){
+        for (int i = 1; i <= n; i++) {
+            Tower currentTower = towers.get(i - 1);
+            float towerCost = currentTower.getCost();
+            float towerValue = getTowerValue(currentTower);
 
-            for(int j=0; j<= (int) money; j++){
-                T[i][j] = -1;
+            for (int j = 0; j <= money; j++) {
+                T[i][j] = T[i - 1][j];
+
+                if (j >= towerCost) {
+                    // Si se puede comprar, se considera la compra de la torreta
+                    T[i][j] = Math.max(T[i][j], T[i - 1][(int) (j - towerCost)] + towerValue);
+                }
             }
         }
 
-        buyTowersDinamic(towers, (int) money, n, T);
-
-        selected.add((int) T[n][(int) money]);
-        return selected;
-    }
-
-    private static void buyTowersDinamic(ArrayList<Tower> towers, int c, int n, float[][] T){
-
-        if(c == 0){
-            T[n][c] = 0;
-
-        }else if (n == 1){
-
-            if(c < towers.get(0).getCost()){
-                T[0][c] = 0;
-
-            }else{
-                T[0][c] = getTowerValue(towers.get(0));
+        // Reconstrucción de las torres seleccionadas en base a T
+        ArrayList<Integer> selected = new ArrayList<>();
+        int i = n;
+        int j = (int) money;
+        while (i > 0 && j > 0) {
+            if (T[i][j] != T[i - 1][j]) {
+                // Torreta seleccionada
+                selected.add(i - 1);
+                j -= towers.get(i - 1).getCost();
             }
-
-        }else if(n > 0){
-
-            if(T[n-1][c] == -1){
-                buyTowersDinamic(towers, c,n - 1, T);
-            }
-
-            if (c - towers.get(n - 1).getCost() >= 0 && T[n][(int) (c - towers.get(n - 1).getCost())] == -1){
-                buyTowersDinamic(towers, (int) (c - towers.get(n).getCost()), n-1, T);
-            }
-            T[n][c] = Math.max(T[n-1][c],T[n-1][c] + getTowerValue(towers.get(n)));
+            i--;
         }
 
+        // hacer un reverse para tener el orden adecuado
+        ArrayList<Integer> reversedSelected = new ArrayList<>();
+        for (int k = selected.size() - 1; k >= 0; k--) {
+            reversedSelected.add(selected.get(k));
+        }
+
+        return reversedSelected;
     }
 
-    public static float getTowerValue(Tower tower){
-
-        return tower.getRange() + 2 * tower.getDispersion();
+    public static float getTowerValue(Tower tower) {
+        return tower.getRange() + 2 * tower.getDispersion() + tower.getDamage();
     }
 }
