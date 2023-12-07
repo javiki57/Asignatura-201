81652621044cf219d6c4b4c8908fab80ae554c00
Javiki <javipatinador@gmail.com>
2023-12-07 20:46:38 +0100

    Práctica 4 con puntuación de 31

--- 2023/P4/net/agsh/towerdefense/strats/TowerPlacer.java
+++ 2023/P4/net/agsh/towerdefense/strats/TowerPlacer.java
@@ -1,6 +1,10 @@
 /*
-Sustituir este comentario por una explicación de la formula o procedimiento empleado para determinar el valor de una
-celda (MapNode).
+El pensamiento para desarrollo de la función getNodeValue ha sido dar un mayor valor a las celdas
+más cercanas al centro del mapa y menos a las más lejanas. Para ello, se ha hecho una función
+matemática que calcula la distancia de la celda pasada por parámetro al centro del mapa, aproximadamente.
+Después, se le asigna un valor a la celda inversamente proporcional a la distancia al centro.
+Además, las torretas que se van a colocar han sido previamente ordenadas descendentemente según su
+radio, por lo que primeramente se añadirán las torretas de mayor radio.
 */
 
 package net.agsh.towerdefense.strats;
@@ -10,32 +14,28 @@ import net.agsh.towerdefense.*;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TowerPlacer {
 
     public static float getNodeValue(MapNode node, Map map) {
 
+        // Obtener las coordenadas del mapa
         float mapWidth = map.getSize().x;
         float mapHeight = map.getSize().y;
 
         // Obtener las coordenadas de la celda
-        float xCoordinate = node.getPosition().x;
-        float yCoordinate = node.getPosition().y;
+        float coordenadaX = node.getPosition().x;
+        float coordenadaY = node.getPosition().y;
 
         // Calcular la distancia al centro del mapa
-        float distanceToEdges = Math.min(Math.min(xCoordinate, mapWidth - xCoordinate), Math.min(yCoordinate, mapHeight - yCoordinate));
+        float distanceToEdges = Math.min(Math.min(coordenadaX, mapWidth - coordenadaX), Math.min(coordenadaY, mapHeight - coordenadaY));
         // Asignar un valor inversamente proporcional a la distancia al centro
-        float value = 5.0f / (distanceToEdges  + 1);
+        float value = 1.0f / (distanceToEdges  + 1);
 
         return value;
     }
 
-    // Método auxiliar para obtener el nodo caminable más cercano a un nodo dado
-    private static MapNode getClosestWalkableNode(MapNode node, ArrayList<MapNode> walkableNodes) {
-        return walkableNodes.stream()
-                .min(Comparator.comparingDouble(walkableNode -> node.getPosition().distance(walkableNode.getPosition())))
-                .orElse(null);
-    }
 
     public static boolean collide(Point2D entity1Position, float entity1Radius,
                                   Point2D entity2Position, float entity2Radius) {
@@ -50,40 +50,21 @@ public class TowerPlacer {
 
     public static ArrayList<Tower> placeTowers(ArrayList<Tower> towers, Map map) {
 
-        // Get map size.
-//        float mapWidth = map.getSize().x;
-//        float mapHeight = map.getSize().y;
-
-
-        // Get parameters from the game.
-//        float maxEnemyRadius = Game.getInstance().getParam(Config.Parameter.ENEMY_RADIUS_MAX);
-//        float maxRange = Game.getInstance().getParam(Config.Parameter.TOWER_RANGE_MAX);
-
         // Get nodes the enemies can walk on.
         ArrayList<MapNode> walkableNodes = map.getWalkableNodes();
 
         // Get obstacles.
         ArrayList<Obstacle> obstacles = map.getObstacles();
 
-//        // Get the grid of map nodes.
-//        MapNode[][] grid = map.getNodes();
-//
-//        // Get the distance between two nodes.
-//        MapNode n1 = grid[0][0];
-//        MapNode n2 = grid[0][1];
-//        float distance = n1.getPosition().distance(n2.getPosition());
 
-
-        // Each node has up to Config.Parameter.MAP_NODE_VALUES values. Default is 5.
-        // You can *optionally* use these values to store information about the nodes.
-        // Set the second value of node n1.
-//        n1.setValue(2, getNodeValue(n1, map));
-//
-//        // Get the second value of node n1.
-//        float value = n1.getValue(2);
 
         // Se crea la lista donde se almacenan las soluciones
         ArrayList<Tower> placedTowers = new ArrayList<>();
+
+        //Se ordenan las torres de mayor a menor radio
+        List<Tower> sortedTowers = towers.stream()
+                .sorted(Comparator.comparingDouble(Tower::getRadius).reversed())
+                .collect(Collectors.toList());
 
         // Obtener los nodos del mapa ordenados de mayor a menor valor, según lo definido en getNodeValue
         List<MapNode> sortedNodes = new ArrayList<>(map.getNodesList().stream()
@@ -92,12 +73,12 @@ public class TowerPlacer {
                 .toList());
 
         //mientras que no sea solución o candidatos != vacío
-        while(!towers.isEmpty() && !sortedNodes.isEmpty()){
+        while(!sortedTowers.isEmpty() && !sortedNodes.isEmpty()){
 
             // Extraer el primer candidato (nodo con mayor valor)
             MapNode candidateNode = sortedNodes.remove(0);
             Point2D candidatePosition = candidateNode.getPosition();
-            Tower tower = towers.remove(0);
+            Tower tower = sortedTowers.remove(0);
 
             if (factible(tower, candidatePosition, placedTowers, obstacles, map)) {
                 // Agregar la torre a la lista de torres colocadas
@@ -105,7 +86,7 @@ public class TowerPlacer {
                 placedTowers.add(tower);
 
             }else{
-                towers.add(tower);
+                sortedTowers.add(tower);
             }
         }
 
